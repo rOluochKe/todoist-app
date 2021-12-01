291d7bb1cce523a2ee3125a5e06e28999dc948d4
roluochKe <rasholuoch@gmail.com>
2021-12-01 21:09:08 +0300

    Updates

---
+++ precache-manifest.aee53113174f84189e201a32bd17f601.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff1bdc9ad168bd00cf84ed73981e03e",
+    "url": "/todoist-app/index.html"
+  },
+  {
+    "revision": "4ee19aaf21625eee4815",
+    "url": "/todoist-app/static/css/main.ba8a0471.chunk.css"
+  },
+  {
+    "revision": "29083acf87e3f826e860",
+    "url": "/todoist-app/static/js/2.89da6355.chunk.js"
+  },
+  {
+    "revision": "d131356b777feba5333d4cbd5180de82",
+    "url": "/todoist-app/static/js/2.89da6355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ac0b6adc89ae8bdd16",
+    "url": "/todoist-app/static/js/3.a6213ffb.chunk.js"
+  },
+  {
+    "revision": "4ee19aaf21625eee4815",
+    "url": "/todoist-app/static/js/main.771cfe99.chunk.js"
+  },
+  {
+    "revision": "a62fcc45a2252dcce23d",
+    "url": "/todoist-app/static/js/runtime-main.c82b55b9.js"
+  }
+]);
